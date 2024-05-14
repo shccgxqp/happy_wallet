2b360637ef2b062a0d2a3285815e68a96f91dc13
shccgxqp <shccgxqp@gmail.com>
2024-05-14 19:31:44 +0800

    後端修改 - 添加暫時app.env

--- backend/api/main_test.go
+++ backend/api/main_test.go
@@ -12,9 +12,9 @@ import (
 )
 
 
-func newTestServer(t *testing.T, store db.Store) *Server{
+func newTestServer(t *testing.T, store db.Store) *Server {
 	config := util.Config{
-		TokenSymmetricKey:  util.RandomString(32),
+		TokenSymmetricKey:   util.RandomString(32),
 		AccessTokenDuration: time.Minute,
 	}
 
@@ -24,7 +24,8 @@ func newTestServer(t *testing.T, store db.Store) *Server{
 	return server
 }
 
-func TestMain(m *testing.M) {	
+func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
+
 	os.Exit(m.Run())
 }
