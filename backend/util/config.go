fa12e56507245a83a166779f74747ebb453305d0
shccgxqp <shccgxqp@gmail.com>
2024-05-16 17:51:41 +0800

    後端新增 - 整合compose，並將mirgration功能改自應用程式內

--- backend/util/config.go
+++ backend/util/config.go
@@ -9,6 +9,7 @@ import (
 type Config struct {
 DB_DRIVER string `mapstructure:"DB_DRIVER"`
 DB_SOURCE string `mapstructure:"DB_SOURCE"`
+MIGRATION_URL string `mapstructure:"MIGRATION_URL"`
 HTTP_SERVER_ADDRESS string `mapstructure:"HTTP_SERVER_ADDRESS"`
 GRPC_SERVER_ADDRESS string `mapstructure:"GRPC_SERVER_ADDRESS"`
 TOKEN_SYMMETRIC_KEY string `mapstructure:"TOKEN_SYMMETRIC_KEY"`
