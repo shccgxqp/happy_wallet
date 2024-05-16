636a54ae47e6a35fce211741873439e497ee27ac
shccgxqp <shccgxqp@gmail.com>
2024-05-16 13:13:16 +0800

    後端新增 - 加入swagger 網頁

--- backend/main.go
+++ backend/main.go
@@ -85,6 +85,9 @@ func runGatewayServer(config util.Config,store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
+	fs := http.FileServer(http.Dir("./doc/swagger"))
+	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+
 	listener, err := net.Listen("tcp", config.HTTP_SERVER_ADDRESS)
     if err != nil {
         log.Fatal("cannot listen to grpc server:", err)
